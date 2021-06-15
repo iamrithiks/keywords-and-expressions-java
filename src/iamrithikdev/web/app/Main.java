bc8c40255c3431f2fe3a9951be1bacad6560132e
Rithik Samanthula <rithikdeveloper754@gmail.com>
2021-06-15 12:51:53 -0400

    Add files via upload

---
+++ src/iamrithikdev/web/app/Main.java
@@ -0,0 +1,18 @@
+package iamrithikdev.web.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // int int = 5; <- 👎🏾
+        // false, true, null
+
+        // a mile is equal to 1.609344 kilometers
+        double totalKilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+    }
+}
